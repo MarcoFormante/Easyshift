c9c4bf1033c93f7fedb833f0f744979087a090b5
marciuto <formante.marco@gmail.com>
2023-02-03 11:04:26 +0100

    adding login processus test

--- Js/loginPage.js
+++ Js/loginPage.js
@@ -1,1 +1,20 @@
+const formLogin = document.querySelector("#form-login");
 
+formLogin.addEventListener("submit", enterToHome);
+
+function enterToHome(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    const name = document.querySelector("#name");
+    const password = document.querySelector("#password");
+
+    userCanAccess = name.value === "marco" && password.value === "marco";
+        
+    if (userCanAccess){
+        document.location.href = "home.html";
+    } else {
+        alert("name or password not exists, try again!");
+    }
+
+    
+}
